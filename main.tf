8f710f16855f9247374ae77140157fe3190730a2
Aravind Chowdary <kamaravichow@gmail.com>
2024-04-04 15:33:43 +0530

    basic

---
+++ main.tf
@@ -0,0 +1,63 @@
+provider "aws" {
+  profile = "default"
+  region  = "ap-south-1"
+}
+
+data "aws_ami" "aws_ubuntu" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn-ami-hvm*"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+resource "aws_instance" "aws_ubuntu" {
+  instance_type = "t2.micro"
+  ami           = "ami-09298640a92b2d12c"
+  user_data     = file("userdata.tpl")
+}
+
+
+resource "aws_default_vpc" "default" {
+
+}
+
+resource "aws_security_group" "demo_sg" {
+  name        = "demo_sg"
+  description = "allow ssh on 22 & http on port 80"
+  vpc_id      = aws_default_vpc.default.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
